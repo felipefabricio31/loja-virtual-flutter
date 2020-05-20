dc74fe4c9c2aa3dbbe42d41cbdce7117294669db
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-20 17:53:11 -0300

    apresentando Lista de categorias.

--- lib/tabs/products_tab.dart
+++ lib/tabs/products_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_virtual/tiles/category_tile.dart';
 
 class ProductsTab extends StatelessWidget {
   @override
@@ -12,7 +13,14 @@ class ProductsTab extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         } else {
-          return ListView();
+          var dividedTiles = ListTile.divideTiles(
+                  tiles: snapshot.data.documents.map((doc) {
+                    return CategoryTile(doc);
+                  }).toList(),
+                  color: Colors.grey[500])
+              .toList();
+
+          return ListView(children: dividedTiles);
         }
       },
     );
