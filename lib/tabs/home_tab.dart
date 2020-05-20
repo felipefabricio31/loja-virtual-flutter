399768f83fbd63840405c891989a68223cfcb868
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-20 12:22:57 -0300

    Add Loadgin para carregar imagens do firebase.

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({Key key}) : super(key: key);
@@ -33,7 +34,36 @@ class HomeTab extends StatelessWidget {
                 title: const Text("Novidades"),
                 centerTitle: true,
               ),
-            )
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection("home")
+                  .orderBy("pos")
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                } else {
+                  print(snapshot.data.documents.length);
+
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: Container(),
+                    ),
+                  );
+                }
+              },
+            ),
           ],
         ),
       ],
