6addd5664e84cfb3978a6f3f6088fc3168d42e1a
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-20 12:59:27 -0300

    Widget para mostrar imagens na tela.

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({Key key}) : super(key: key);
@@ -54,16 +56,24 @@ class HomeTab extends StatelessWidget {
                 } else {
                   print(snapshot.data.documents.length);
 
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200.0,
-                      alignment: Alignment.center,
-                      child: Container(),
-                    ),
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map((doc) {
+                      return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                    }).toList(),
+                    children: snapshot.data.documents.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc.data["img"],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
                   );
                 }
               },
-            ),
+            )
           ],
         ),
       ],
