34a4b8ad3c222179fa97eea5803b043c88bc3656
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-20 11:47:18 -0300

    Add PageView para navegação entre as páginas.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return PageView();
+    return PageView(
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.red,
+        ),
+      ],
+    );
   }
 }
