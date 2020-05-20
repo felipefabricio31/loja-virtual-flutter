c9f2d457fbce5becf26e2aeb05d9dfb80ecbc8fa
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-20 18:30:22 -0300

    Grade da lista de produtos.

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/datas/produtos_data.dart';
+import 'package:loja_virtual/tiles/product_tile.dart';
+
+class CategoryScreen extends StatelessWidget {
+  final DocumentSnapshot snapshot;
+  CategoryScreen(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(snapshot.data["title"]),
+            centerTitle: true,
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.grid_on),
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                ),
+              ],
+            ),
+          ),
+          body: FutureBuilder<QuerySnapshot>(
+            //Retorna a lista de categorias
+            future: Firestore.instance
+                .collection("products")
+                .document(snapshot.documentID)
+                .collection("items")
+                .getDocuments(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                //loading
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+                //
+              } else {
+                TabBarView(
+                  physics: NeverScrollableScrollPhysics(),
+                  children: <Widget>[
+                    //Tab 01 - grid
+                    GridView.builder(
+                        padding: EdgeInsets.all(4.0),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 4.0,
+                          crossAxisSpacing: 4.0,
+                          childAspectRatio: 0.65,
+                        ),
+                        itemCount: snapshot.data.documents.length,
+                        itemBuilder: (context, index) {
+                          return ProductTile(
+                              "grid",
+                              ProductData.fromDocument(
+                                  snapshot.data.documents[index]));
+                        }),
+                    //Tab 02 - Lista
+                    ListView.builder(
+                        padding: EdgeInsets.all(4.0),
+                        itemCount: snapshot.data.documents.length,
+                        itemBuilder: (context, index) {
+                          return ProductTile(
+                              "list",
+                              ProductData.fromDocument(
+                                  snapshot.data.documents[index]));
+                        }),
+                  ],
+                );
+              }
+            },
+          )),
+    );
+  }
+}
