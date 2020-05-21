2a799d1b8f35f35f5dc5a40dce3ee99b9b65a687
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-21 11:09:04 -0300

    Finalizando a tela de produto.

--- lib/screens/product.screen.dart
+++ lib/screens/product.screen.dart
@@ -99,6 +99,32 @@ class _ProductScreenState extends State<ProductScreen> {
                     }).toList(),
                   ),
                 ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    onPressed: size != null ? () {} : null,
+                    child: Text(
+                      "Adicionar ao carrinho",
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    color: primaryColor,
+                    textColor: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  "Descrição",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16.0),
+                ),
               ],
             ),
           )
