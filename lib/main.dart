d60f44569f73471a68dcadb05ce9cd2ed7358288
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-21 11:23:23 -0300

    Criando a tela de login.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/screens/home_screen.dart';
+import 'package:loja_virtual/screens/login_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Color.fromARGB(255, 4, 125, 141),
       ),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Entrar"),
+        centerTitle: true,
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              "CRIAR CONTA",
+              style: TextStyle(fontSize: 15.0),
+            ),
+            textColor: Colors.white,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(hintText: "E-mail"),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text) {
+                if (text.isEmpty || !text.contains("@")) {
+                  return "Email inválido!";
+                }
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              decoration: InputDecoration(hintText: "Senha"),
+              obscureText: true,
+              validator: (text) {
+                if (text.isEmpty || text.length < 6) {
+                  return "Senha inválida!";
+                }
+              },
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Esqueci minha senha",
+                  textAlign: TextAlign.right,
+                ),
+                padding: EdgeInsets.zero,
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            SizedBox(
+              height: 44.0,
+              child: RaisedButton(
+                child: Text(
+                  "Entrar",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                textColor: Colors.white,
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  if(_formKey.currentState.validate()){
+                    
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
