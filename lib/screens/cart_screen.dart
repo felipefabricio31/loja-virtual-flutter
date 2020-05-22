d85c4ce705bcbb30f06f157fe9599968fdc94111
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-22 11:36:51 -0300

    Recuperando dados do produto - 163

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
+import 'package:loja_virtual/tiles/cart_tile.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 import 'login_screen.dart';
@@ -79,6 +80,16 @@ class CartScreen extends StatelessWidget {
                 "Nenhum produto no carrinho",
                 style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
               ),
+            );
+          } else {
+            return ListView(
+              children: <Widget>[
+                Column(
+                  children: model.products.map((product) {
+                    return CartTile(product);
+                  }).toList(),
+                )
+              ],
             );
           }
         },
---
+++ lib/tiles/cart_tile.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/datas/cart_product.dart';
+import 'package:loja_virtual/datas/produtos_data.dart';
+
+class CartTile extends StatelessWidget {
+  final CartProduct cartProduct;
+  const CartTile(this.cartProduct);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildContent() {
+      return 
+    }
+
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: cartProduct.productData == null
+          ? FutureBuilder<DocumentSnapshot>(
+              future: Firestore.instance
+                  .collection("products")
+                  .document(cartProduct.category)
+                  .collection("items")
+                  .document(cartProduct.pid)
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  cartProduct.productData =
+                      ProductData.fromDocument(snapshot.data);
+                  return _buildContent();
+                } else {
+                  return Container(
+                    height: 70.0,
+                    child: CircularProgressIndicator(),
+                    alignment: Alignment.center,
+                  );
+                }
+              },
+            )
+          : _buildContent(),
+    );
+  }
+}
